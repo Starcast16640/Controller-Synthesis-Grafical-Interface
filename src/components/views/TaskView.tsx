241997c3d89a41057207a759e7b736f84093b148
Starcast16640 <starcast16640@gmail.com>
2026-04-07 17:03:20 +0200

    Updated TaskView.tsx

--- src/components/views/TaskView.tsx
+++ src/components/views/TaskView.tsx
@@ -200,12 +200,13 @@ export function TaskView() {
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Final Condition
                 </th>
-                <th className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Actions
-                </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Address
                 </th>
+                <th className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Actions
+                </th>
+                
               </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
