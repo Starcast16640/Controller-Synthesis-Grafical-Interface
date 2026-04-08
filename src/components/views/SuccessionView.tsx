82ddf520de7380d4e457d612e31b054e490fb1ab
Starcast16640 <starcast16640@gmail.com>
2026-04-08 09:11:15 +0200

    Updated SuccessionView.tsx

--- src/components/views/SuccessionView.tsx
+++ src/components/views/SuccessionView.tsx
@@ -260,7 +260,7 @@ export function SuccessionView() {
             onClick={handleCreateNode}
             className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
           >
-            Add Nod
+            Add Node
           </button>
           <button
             onClick={() => {
