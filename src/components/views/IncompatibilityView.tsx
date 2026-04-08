09d532920a3491f9433e7c6e3ea840c380a22c48
Starcast16640 <starcast16640@gmail.com>
2026-04-08 14:52:17 +0200

    Updated IncompatibilityView.tsx

--- src/components/views/IncompatibilityView.tsx
+++ src/components/views/IncompatibilityView.tsx
@@ -163,7 +163,7 @@ export function IncompatibilityView() {
             <select
               value={task1Id}
               onChange={(e) => setTask1Id(e.target.value)}
-              className=""
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-red-500 focus:border-red-500 bg-white"
             >
               <option value="">Select Task</option>
               {tasks.map((t) => (
