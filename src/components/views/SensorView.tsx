c244a7ec83b0874efccf73c4d9b44297741d2912
Starcast16640 <starcast16640@gmail.com>
2026-04-08 09:02:28 +0200

    Updated SensorView.tsx

--- src/components/views/SensorView.tsx
+++ src/components/views/SensorView.tsx
@@ -79,7 +79,7 @@ export function SensorView() {
             />
             <input
               type="text"
-              placeholder="Address de Mapping"
+              placeholder="Address Mapping"
               value={formData.factory_io_address || ''}
               onChange={(e) => setFormData({ ...formData, factory_io_address: e.target.value })}
               className="px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
