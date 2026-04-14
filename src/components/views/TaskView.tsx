c4590a211c9018b3458c71519c7e837fd1c752ce
Starcast16640 <starcast16640@gmail.com>
2026-04-14 11:48:30 +0200

    Updated TaskView.tsx

--- src/components/views/TaskView.tsx
+++ src/components/views/TaskView.tsx
@@ -22,7 +22,7 @@ export function TaskView() {
     name: '',
     type: ['simple'],
     authorization_expression: '',
-    final_condition: 'AUTO',
+    final_condition: '',
     max_simultaneous_executions: 1,
     priority: 0,
     factory_io_address: '',
@@ -86,7 +86,7 @@ export function TaskView() {
       name: '',
       type: ['simple'],
       authorization_expression: '',
-      final_condition: 'AUTO',
+      final_condition: '',
       max_simultaneous_executions: 1,
       priority: 0,
       factory_io_address: ''
