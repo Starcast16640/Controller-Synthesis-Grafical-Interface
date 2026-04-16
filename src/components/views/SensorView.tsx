a6b8964468a4f7d609b5ca6325d5ae5c0b86347e
Starcast16640 <starcast16640@gmail.com>
2026-04-16 09:37:58 +0200

    Updated SensorView.tsx

--- src/components/views/SensorView.tsx
+++ src/components/views/SensorView.tsx
@@ -68,7 +68,7 @@ export function SensorView() {
               <select
                 value={formData.type || 'Boolean'}
                 onChange={(e) => setFormData({ ...formData, type: e.target.value as Sensor['type'] })}
-                className="px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               >
                 <option value="Boolean">Boolean</option>
                 <option value="Integer">Integer</option>
