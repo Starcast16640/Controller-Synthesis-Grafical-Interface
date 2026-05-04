eea08c1194a7ec2f79a9e4b40beb9c1b6517e9aa
Starcast16640 <starcast16640@gmail.com>
2026-05-04 10:26:57 +0200

    Updated SensorView.tsx

--- src/components/views/SensorView.tsx
+++ src/components/views/SensorView.tsx
@@ -88,7 +88,7 @@ export function SensorView() {
                   e.target.setCustomValidity("");
                   setFormData({ ...formData, name: e.target.value });
                 }}
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                 required
               />
             </div>
