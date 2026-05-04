574ae3570ef753a109c4bc095d23e6838e505052
Starcast16640 <starcast16640@gmail.com>
2026-05-04 14:53:39 +0200

    Updated ObserverView.tsx

--- src/components/views/ObserverView.tsx
+++ src/components/views/ObserverView.tsx
@@ -175,7 +175,7 @@ export function ObserverView() {
                 <select
                   value={formData.type}
                   onChange={(e) => handleTypeChange(e.target.value as ObserverType)}
-                  className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+                  className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                 >
                   <option value="expression">Expression</option>
                   <option value="counter">Counter</option>
