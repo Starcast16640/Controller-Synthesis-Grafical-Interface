1de07a30acfbe122183b92ddde629ec024fdcfa0
Starcast16640 <starcast16640@gmail.com>
2026-05-07 13:48:36 +0200

    Updated SuccessionView.tsx

--- src/components/views/SuccessionView.tsx
+++ src/components/views/SuccessionView.tsx
@@ -717,6 +717,9 @@ return (
             );
           })}
           </div>
+        </div>
+      </>
+    )}
       {editingNode && (
         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white rounded-lg shadow-lg p-6 max-w-md w-full">
@@ -850,7 +853,6 @@ return (
               </button>
             </div>
           </div>
-        </div>
       )}
     </div>
   );
